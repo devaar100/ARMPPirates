8c8e356175153892ef980e872069e7391dd93ea0
devaar100 <developer.aarnav100@gmail.com>
2018-12-05 23:47:01 +0530

    Initial setup done

---
+++ Assets/Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Health : NetworkBehaviour{
+
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount){
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0){
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+    }
+
+    void OnChangeHealth(int health){
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+}
